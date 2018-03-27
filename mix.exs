af5cfd969976cf502e6e68b7b5df550173a45acb
Frank Hunleth <fhunleth@troodon-software.com>
2018-03-27 18:31:33 -0400

    Remove dialyzer from deps; install locally to use

--- mix.exs
+++ mix.exs
@@ -30,8 +30,7 @@ defmodule Nerves.Firmware.SSH.Mixfile do
   defp deps() do
     [
       {:nerves_runtime, "~> 0.4"},
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
